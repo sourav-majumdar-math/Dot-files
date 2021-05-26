7154eb88c09d92259e2e8fa5a16c0033034b2fd5
sourav-majumdar-math <phd18souravm@iima.ac.in>
2021-05-26 21:19:38 +0530

    Font size changes

--- dwmblocks/blocks/nifty_button.sh
+++ dwmblocks/blocks/nifty_button.sh
@@ -12,5 +12,5 @@ result_nifty_next_50=$(sed 's/\^NSMIDCP/NIFTY N50/g'<<<"$NIFTY_NEXT_50")
 
 TERMINAL="alacritty"
 
-exec $TERMINAL -t "Notification-Market" --hold -o colors.cursor.cursor=CellBackground background_opacity=1 -e printf "\n\n\n %b\n %b\n %b" "$result" "$result_nifty_next_50" "$result_nasdaq" 
+exec $TERMINAL -t "Notification-Market" --hold -o colors.cursor.cursor=CellBackground background_opacity=1 font.size=11.0 -e printf "\n\n\n %b\n %b\n %b" "$result" "$result_nifty_next_50" "$result_nasdaq" 
 
