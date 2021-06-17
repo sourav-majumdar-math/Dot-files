daedba5265ec06fe63abc19800ed51f0b6ea8bde
sourav-majumdar-math <phd18souravm@iima.ac.in>
2021-06-17 05:35:38 +0530

    battery.sh typo

--- dwmblocks/blocks/battery.sh
+++ dwmblocks/blocks/battery.sh
@@ -7,17 +7,17 @@ read -r charge </sys/class/power_supply/BAT1/status
 if [[ $charge == "Charging" ]]; then
 	ICON="Charging"
 elif [[ $charge == "Full" ]]; then
-	ICON="Full"
+	ICON="Charging"
 elif [[ $capacity -gt 75 ]]; then
-	ICON=""
+	ICON="BAT"
 elif [[ $capacity -le 75 && $capacity -gt 50 ]]; then
-	ICON=""
+	ICON="BAT"
 elif [[ $capacity -le 50 && $capacity -gt 30 ]]; then
-	ICON=""  	
+	ICON="BAT"  	
 elif [[ $capacity -le 30 && $capacity -gt 10 ]]; then
-	ICON="Low"
+	ICON="BAT"
 else 
-	ICON="Low"  
+	ICON="BAT"  
 fi
 
 printf "| %s %3s%% " "$ICON" "$capacity"
